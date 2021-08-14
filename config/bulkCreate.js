6645dee32bb7dc8fdb7196bd4b84695c057ba51c
nicolaszuleta95 <nicolaszuleta95@gmail.com>
2021-08-14 11:57:39 -0500

    validation for the bulkCreate function is fixed

--- config/bulkCreate.js
+++ config/bulkCreate.js
@@ -1,30 +1,35 @@
 const UserModel = require("../models/users");
 
 /**
- * TODO: Fix the validation so that wrong data wont populate the database
+ * This function creates records for each table in bulk
  */
 
 function bulkCreate() {
-  UserModel.bulkCreate([
-    {
-      username: "admin",
-      password: "admin123",
-      email: "admin@admin.com",
-      fullname: "administrador admin",
-      address: "calle Admin",
-      phone: "1234567",
-      user_type: "blabla", //! this is an error validation should work and its not working
-    },
-    {
-      username: "cliente1",
-      password: "cliente123",
-      email: "cliente@cliente.com",
-      fullname: "cliente cliente1",
-      address: "calle Cliente",
-      phone: "1234567",
-      user_type: "user",
-    },
-  ]);
+  UserModel.bulkCreate(
+    [
+      {
+        username: "admin",
+        password: "admin123",
+        email: "admin@admin.com",
+        fullname: "administrador admin",
+        address: "calle Admin",
+        phone: "1234567",
+        user_type: "admin",
+      },
+      {
+        username: "cliente1",
+        password: "cliente123",
+        email: "cliente@cliente.com",
+        fullname: "cliente cliente1",
+        address: "calle Cliente",
+        phone: "1234567",
+        user_type: "user",
+      },
+    ],
+    { validate: true }
+  ).catch((errors) => {
+    console.log(errors);
+  });
 }
 
 module.exports = bulkCreate;
