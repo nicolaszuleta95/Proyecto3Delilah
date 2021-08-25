45109b76f66b67b2b2a418ce23bd14ab65514748
nicolaszuleta95 <nicolaszuleta95@gmail.com>
2021-08-25 16:21:19 -0500

    basic authorization now working

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,3 +1,13 @@
+const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 
-module.exports = (req, res, next) => {};
+module.exports = (req, res, next) => {
+  console.log(req.auth);
+  const { user_ID, user_type } = req.auth;
+
+  if (user_type === "admin") {
+    return next();
+  } else {
+    return res.status(403).send({ error: "Not authorized" });
+  }
+};
