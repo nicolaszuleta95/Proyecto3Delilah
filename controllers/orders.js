b14bc688b71efae1e8047724165e49a76c7d2ff2
nicolaszuleta95 <nicolaszuleta95@gmail.com>
2021-09-09 11:06:13 -0500

    Getting all the orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -29,7 +29,40 @@ exports.addOrder = async (req, res) => {
   }
 };
 
-exports.getOrders = async (req, res) => {};
+exports.getOrders = async (req, res) => {
+  const { user_ID, user_type } = req.auth;
+
+  const query = {};
+  query.where = { user_ID: user_ID };
+
+  if (user_type === "admin") {
+    try {
+      Order.findAll().then((order) => {
+        if (order.length == 0) {
+          res.send("No Orders stored");
+        }
+        res.json(order);
+      });
+    } catch (err) {
+      res.status(400).send({ error: "Error getting all the Orders" });
+    }
+  } else if (user_type === "user") {
+    try {
+      Order.findAll(query).then((order) => {
+        if (order.length == 0) {
+          res.send("No Orders stored for that user");
+        }
+        res.json(order);
+      });
+    } catch (error) {
+      res.status(400).send({ error: "Error getting all the Orders" });
+    }
+  } else {
+    res
+      .status(400)
+      .send({ error: "Not able to get the Orders for this type of user" });
+  }
+};
 
 exports.getOrder = async (req, res) => {};
 
